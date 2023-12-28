6657da2dbdc7b5513d43885711d8afd840368f79
Paul Asalgado <locopol@gmail.com>
2023-12-27 22:05:29 -0300

    Update LMToNC.js
    Redesign code to download from command line, arguments added, syntax added.

--- LMToNC.js
+++ LMToNC.js
@@ -1,66 +1,92 @@
-const ID = 18854;
-
 const LIBRARIES = {
   FS: require("fs"),
   GIF_FRAMES: require("gif-frames"),
   FRAME: require("./public/js/Frame.js"),
   HTTPS: require("https"),
   PNG: require('png-js'),
-  PATH: require('path')
+  PATH: require('path'),
+  BUFFER: require('node:buffer'),
+  ARGS: require('node:process')
 };
 
-const TEMP_FOLDER = "temp";
+var retval = 0;
 let frames_png = [];
+let ID = 0;
+let icon = 0;
+let output = '';
+let FILE_PATH = '.\\public\\js\\icons\\';
+let BASE_URL = 'https://developer.lametric.com/content/apps/icon_thumbs/';
 
-LIBRARIES.HTTPS.get("https://developer.lametric.com/content/apps/icon_thumbs/" + ID + ".gif", (res) => {
-  const TEMP_FILE_NAME = LIBRARIES.PATH.join(TEMP_FOLDER, "temp.gif");
-  // Open file in local filesystem
-  LIBRARIES.FS.mkdirSync(TEMP_FOLDER);
-  const FILE = LIBRARIES.FS.createWriteStream(TEMP_FILE_NAME);
+if (process.argv.length==6) { 
+   (process.argv[2] == '-id') ? ID=process.argv[3] : retval=1; 
+   (process.argv[4] == '-icon') ? icon=process.argv[5] : retval=1; 
+} 
+else { retval = 1; }
 
-  // Write data into local file
-  res.pipe(FILE);
+if (retval == 1) { 
+  console.log('Invalid Arguments: example: <node.exe> <LMtoNC.js> -id <Icon ID> -icon "Short_Icon_Name"');
+  process.exit(1); 
+} 
+else { getIcons(BASE_URL,ID,'.gif'); }
 
-  // Close the file
-  FILE.on("finish", () => {
-    FILE.close();
-    console.log('"call": new Icon([');
+function getIcons(url, ID, ext) {
+req = LIBRARIES.HTTPS.get(url+ID+ext, (res) => {
+	res.on('data', (d) => {     
+    const imgbuf = Buffer.from(d);
 
-    LIBRARIES.GIF_FRAMES({ url: TEMP_FILE_NAME, frames: "all", outputType: "png" },
-      function (err, frameData) {
-        if (err) {
-          throw err;
-        }
+  if(ext == '.png') {
+    frames_png.push(imgbuf);
+    PNGToGrid();
+  }
 
-        frameData.forEach(function (frame) {
-          const TEMP_SMALL_FILE_NAME = LIBRARIES.PATH.join(TEMP_FOLDER, "temp-" + (1000 + frame.frameIndex) + ".png");
-          let stream = frame.getImage().pipe(LIBRARIES.FS.createWriteStream(TEMP_SMALL_FILE_NAME));
+  if(ext == '.gif') {
+    LIBRARIES.GIF_FRAMES({ url: imgbuf, frames: "all", outputType: "png" }, function (err, frameData) {
+      if (err) { throw err; }
 
-          stream.on("finish", function () {
-            frames_png.push(TEMP_SMALL_FILE_NAME);
+      frameData.forEach(function (frame) {
+        var stream = frame.getImage();
+        var bufs = [];
+          stream.on('data', function(d){ bufs.push(d); });
+          stream.on('end',  function( ){ 
+            var buf = Buffer.concat(bufs);
+            frames_png.push(buf);
+
             if(frames_png.length == frameData.length){
               frames_png.sort();
               PNGToGrid();
             }
           });
-        });
-      }
-    );
-
-  });
-}).on("error", (err) => {
-  console.log("Error: ", err.message);
+      });    
+    });
+  } 
+  }); 
+})
+.on("error", (err) => { console.log("Error: ", err.code); })
+.on("response", (res) => { 
+  if(res.statusCode == 404) {
+    if (ext == '.gif') {
+      req.abort();
+      getIcons(BASE_URL,ID,'.png');
+    } else {
+      console.log('Error: Icon not found.');
+      req.abort();
+      process.exit(1);
+    }
+  }
 });
+}
 
 function PNGToGrid(){
+  output += 'DICTIONNARY.' + icon + ' = new Icon([' + "\n";
   Loop(frames_png, 0, function(){
-    console.log("]),");
-    LIBRARIES.FS.rmSync(TEMP_FOLDER, { recursive: true });
+    output += ']);' + "\n";
+    LIBRARIES.FS.writeFileSync(FILE_PATH + icon + '.js', output);
+    console.log('Success: Icon created on "' + FILE_PATH + icon + '.js"');
   });
 }
 
 function Loop(array, index, callback){
-  LIBRARIES.PNG.decode(array[index], function(pixels) {
+  new LIBRARIES.PNG(array[index]).decode(function(pixels) {
     let f = new LIBRARIES.FRAME([
       [],[],[],[],[],[],[],[]
     ]);
@@ -69,21 +95,20 @@ function Loop(array, index, callback){
       const Y = Math.floor(Math.floor(i/4)/8);
       f.grid[Y].push("(" + pixels[i + 0] + "," + pixels[i + 1] + "," + pixels[i + 2] + ")");
     }
-    console.log("  new Frame([");
-    console.log("    " + JSON.stringify(f.grid[0]) + ",");
-    console.log("    " + JSON.stringify(f.grid[1]) + ",");
-    console.log("    " + JSON.stringify(f.grid[2]) + ",");
-    console.log("    " + JSON.stringify(f.grid[3]) + ",");
-    console.log("    " + JSON.stringify(f.grid[4]) + ",");
-    console.log("    " + JSON.stringify(f.grid[5]) + ",");
-    console.log("    " + JSON.stringify(f.grid[6]) + ",");
-    console.log("    " + JSON.stringify(f.grid[7]));
-    console.log("  ], 100),");
+    output += '  new Frame([' + "\n";
+    
+    for(let x = 0; x < 8; x++){
+      let str = "    ";
+      (x==8) ? str += JSON.stringify(f.grid[x]) : str += JSON.stringify(f.grid[x]) + ",";
+      output += str + "\n";
+    }
 
     if(index == array.length - 1){
+      output += '  ], 100)' + "\n";
       callback();
     }
     else{
+      output += '  ], 100),' + "\n";
       Loop(array, index + 1, callback);
     }
   });
